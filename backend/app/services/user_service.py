4d64cda51b42358592a518ca138cd048a8ee9c9d
foiv <t8210126@aueb.gr>
2025-04-25 17:37:40 +0300

    Refactor delete_user method to remove meeting deletion logic

--- backend/app/services/user_service.py
+++ backend/app/services/user_service.py
@@ -46,24 +46,12 @@ class UserService:
         return self.db.get_user(email)
 
     def delete_user(self, email):
-        """Delete a user and all meetings created by them"""
+        """Delete a user"""
 
         # Check if user exists
         user = self.get_user(email)
         if not user:
             return None
-
-        # Get all meetings created by this user
-        user_meetings = self.db.get_meetings_by_user(email)
-        meeting_service = MeetingService()
-
-        # Delete each meeting
-        if user_meetings:
-            for meeting in user_meetings:
-                meeting_id = meeting.get('meeting_id')
-                if meeting_id:
-                    # Delete the meeting
-                    meeting_service.delete_meeting(meeting_id)
 
         # Delete the user
         result = self.db.delete_user(email)
